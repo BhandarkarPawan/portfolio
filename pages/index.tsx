a6ce04dbb1048a1df5899e637f825f238153f19a
Pawan Bhandarkar <pawankbhandarkar@gmail.com>
2021-08-26 23:34:44 +0530

    make page responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,24 +7,26 @@ const Home = () => {
   return (
     <main>
       <Header />
-      <div className="w-1/6 mt-24 md:mx-24 mx-12">
-        <Image
-          className="rounded-full"
-          src="/images/ProfilePhoto.png"
-          alt="Pawan Bhandarkar"
-          height={160}
-          width={160}
-        />
-        <h2 className="mt-4 text-xl">Pawan Bhandarkar</h2>
-        <p className="mt-4">Crohn's Combatant</p>
-        <p className="mt-2">
-          I’m on the road to becoming a full-stack web developer and I share my
-          findings as I go
-        </p>
-        <p className="mt-4">日本語勉強中です</p>
-        <hr className="border-b-1 mt-3 border-solid border-gray-500" />
-        <br />
-        <p className="tracking-wider text-sm"> SOCIALS</p>
+      <div className="md:block flex justify-center w-full items-center">
+        <div className="md:w-1/6 mt-24 md:mx-24 mx-12">
+          <Image
+            className="rounded-full"
+            src="/images/ProfilePhoto.png"
+            alt="Pawan Bhandarkar"
+            height={160}
+            width={160}
+          />
+          <h2 className="mt-4 text-xl">Pawan Bhandarkar</h2>
+          <p className="mt-4">Crohn's Combatant</p>
+          <p className="mt-2">
+            I’m on the road to becoming a full-stack web developer and I share
+            my findings as I go
+          </p>
+          <p className="mt-4">日本語勉強中です</p>
+          <hr className="border-b-1 mt-3 border-solid border-gray-500" />
+          <br />
+          <p className="tracking-wider text-sm"> SOCIALS</p>
+        </div>
       </div>
       <Footer />
     </main>
