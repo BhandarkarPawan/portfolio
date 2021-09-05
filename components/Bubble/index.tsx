9c26f96783947321c5da2a9415a8f38378e5c11a
Pawan Bhandarkar <pawankbhandarkar@gmail.com>
2021-09-05 22:24:19 +0530

    Organize Code a bit better

--- components/Bubble/index.tsx
+++ components/Bubble/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 export enum BubbleColor {
-  PINK = 'pink',
-  GRAY = 'gray',
-  YELLOW = 'yellow',
-  BLUE = 'blue',
+  PINK = 'PINK',
+  GRAY = 'GRAY',
+  YELLOW = 'YELLOW',
+  BLUE = 'BLUE',
 }
 
 export enum BubbleSize {
-  SMALL = 4,
-  MEDIUM = 12,
-  LARGE = 24,
+  SMALL = 'SMALL',
+  MEDIUM = 'MEDIUM',
+  LARGE = 'LARGE',
 }
 
 interface Props {
@@ -19,24 +19,62 @@ interface Props {
   className?: string;
 }
 
+const getBubbleColors = (color: BubbleColor) => {
+  switch (color) {
+    case BubbleColor.PINK:
+      return {
+        outer: 'bg-pink-200',
+        inner: 'bg-pink-500',
+      };
+    case BubbleColor.GRAY:
+      return {
+        outer: 'bg-gray-200',
+        inner: 'bg-gray-500',
+      };
+    case BubbleColor.YELLOW:
+      return {
+        outer: 'bg-yellow-200',
+        inner: 'bg-yellow-500',
+      };
+    case BubbleColor.BLUE:
+      return {
+        outer: 'bg-blue-200',
+        inner: 'bg-blue-500',
+      };
+  }
+};
+
+const getBubbleSizes = (size: BubbleSize) => {
+  switch (size) {
+    case BubbleSize.SMALL:
+      return {
+        outer: 'w-6 h-6 md:w-12 md:h-12',
+        inner: 'md:w-4 md:h-4 w-2 h-2',
+      };
+    case BubbleSize.MEDIUM:
+      return {
+        outer: 'w-12 h-12 md:w-20 md:h-20 ',
+        inner: ' w-4 h-4 md:w-12 md:h-12',
+      };
+    case BubbleSize.LARGE:
+      return {
+        outer: 'w-24 h-24 md:w-32 md:h-32',
+        inner: 'w-12 h-12 md:w-20 md:h-20 ',
+      };
+  }
+};
+
 const Bubble: React.FC<Props> = (props) => {
   const className = props.className || '';
-  const outerColor = `bg-${props.color}-200`;
-  const innerColor = `bg-${props.color}-500`;
+  const { outer: outerColor, inner: innerColor } = getBubbleColors(props.color);
+  const { outer: outerSize, inner: innerSize } = getBubbleSizes(props.size);
 
-  const inner = props.size;
-  const outer = props.size + 8;
-  const innerMD = inner + 8;
-  const outerMD = outer + 8;
-
-  const innerSize = `h-${inner} w-${inner} md:h-${innerMD} md:w-${innerMD}`;
-  const outerSize = `h-${outer} w-${outer} md:h-${outerMD} md:w-${outerMD}`;
+  const outerCircleClass = `z-30 bubble absolute rounded-full outer-circle ${className} ${outerSize} ${outerColor}`;
+  const innerCircleClass = `rounded-full ${innerColor} ${innerSize}`;
 
   return (
-    <div
-      className={`z-30 bubble floating-circle outer-circle ${className} ${outerSize} ${outerColor}`}
-    >
-      <div className={`floating-circle ${innerColor} ${innerSize}`}></div>
+    <div className={outerCircleClass}>
+      <div className={innerCircleClass}></div>
     </div>
   );
 };
--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -10,155 +10,37 @@ const Intro = () => {
       justify-center 
       relative "
     >
-      <div
-        className="
-          z-30 
-          bubble 
-          floating-circle 
-          outer-circle 
-          md:left-12 left-8 
-          md:top-48 top-0 
-          md:w-32 md:h-32 w-24 h-24 
-        bg-gray-200"
-      >
-        <div
-          className="
-            floating-circle 
-            md:w-20 w-12 h-12 md:h-20 
-            bg-gray-500"
-        ></div>
-      </div>
-      <div
-        className="
-          z-30 
-          bubble 
-          floating-circle 
-          outer-circle 
-          md:left-56 right-24
-          md:top-96 top-0 
-          md:w-12 md:h-12 w-6 h-6 
-        bg-gray-200"
-      >
-        <div
-          className="
-            floating-circle 
-            md:w-4 md:h-4 w-2 h-2 
-          bg-gray-500"
-        ></div>
-      </div>
-      <div
-        className="
-          z-30 
-          bubble 
-          floating-circle 
-          outer-circle 
-          md:left-12 right-8 
-          md:bottom-16 
-          md:top-auto top-16 
-          md:w-20 md:h-20 w-12 h-12 
-        bg-gray-200"
-      >
-        <div
-          className="
-            floating-circle 
-            md:w-12 md:h-12 w-4 h-4  
-          bg-gray-500"
-        ></div>
-      </div>
-      <div
-        className="
-          z-30 
-          bubble 
-          floating-circle 
-          outer-circle 
-          md:right-24 md:bo
-          ttom-32 top-40 
-          right-24 
-          md:w-20 md:h-20 w-12 h-12 
-        bg-gray-200"
-      >
-        <div
-          className="
-            floating-circle 
-            md:w-12 md:h-12 w-4 h-4
-          bg-gray-500"
-        ></div>
-      </div>
-
-      <div
-        className="
-          z-50 
-          md:w-1/3 md:h-auto h-48 
-          md:text-left text-center 
-          flex flex-col 
-          justify-center"
-      >
-        <h1
-          className="
-            mt-4 
-            md:text-5xl text-4xl "
-        >
-          Hi.
-        </h1>
-        <h1
-          className="
-            mt-4 
-            md:text-5xl text-4xl "
-        >
-          I'm Pawan
-        </h1>
-        <p
-          className="
-            mt-2 
-            md:text-xl text-md  
-            tracking-wider"
-        >
-          A Web Developer
-        </p>
+      <Bubble
+        color={BubbleColor.GRAY}
+        size={BubbleSize.LARGE}
+        className="md:left-12 left-8 md:top-48 top-0"
+      />
+      <Bubble
+        color={BubbleColor.GRAY}
+        size={BubbleSize.SMALL}
+        className="md:left-56 right-24 md:top-96 top-0"
+      />
+      <Bubble
+        color={BubbleColor.GRAY}
+        size={BubbleSize.MEDIUM}
+        className="md:left-12 right-8 md:bottom-16 md:top-auto top-16"
+      />
+      <Bubble
+        color={BubbleColor.GRAY}
+        size={BubbleSize.MEDIUM}
+        className="md:right-24 right-24 md:bottom-32 top-40 
+        "
+      />
+      <div className="z-50 md:w-1/3 md:h-auto h-48 md:text-left text-center flex flex-col justify-center">
+        <h1 className="title">Hi.</h1>
+        <h1 className="title">I'm Pawan</h1>
+        <p className="subtitle">A Web Developer</p>
       </div>
       <div className="relative">
-        <div
-          className="
-            z-20   
-            bubble 
-            floating-circle 
-            left-4 
-            lg:w-96 lg:h-96 sm:w-72 sm:h-72 h-56 w-56 
-            bg-gray-500"
-        ></div>
-        <div
-          className="
-            z-30 
-            bubble 
-            md:bold-ring 
-            floating-circle 
-            md:left-0 left-4 
-            md:bottom-72 bottom-48 
-            md:w-32 md:h-32 sm:w-20 sm:h-20 h-16 w-16 
-            border-8 border-yellow-400 "
-        ></div>
-        <div
-          className="
-            z-30
-            bubble 
-            md:semibold-ring 
-            floating-circle  
-            left-4
-            md:bottom-56 bottom-40  
-            md:w-12 md:h-12 h-6 w-6 
-            border-8 border-pink-500 "
-        ></div>
-        <div
-          className="
-            z-30
-            bubble 
-            md:semibold-ring 
-            floating-circle  
-            md:left-20 left-12 
-            md:bottom-48 bottom-32 
-            md:w-20 md:h-20 h-10 w-10 
-            border-8 border-blue-500 "
-        ></div>
+        <div className="z-20 bubble floating-circle left-4 lg:w-96 lg:h-96 sm:w-72 sm:h-72 h-56 w-56 bg-gray-500"></div>
+        <div className="z-30 bubble md:bold-ring floating-circle md:left-0 left-4 md:bottom-72 bottom-48 md:w-32 md:h-32 sm:w-20 sm:h-20 h-16 w-16 border-8 border-yellow-400 "></div>
+        <div className="z-30 bubble md:semibold-ring floating-circle left-4 md:bottom-56 bottom-40  md:w-12 md:h-12 h-6 w-6 border-8 border-pink-500 "></div>
+        <div className="z-30 bubble md:semibold-ring floating-circle md:left-20 left-12 md:bottom-48 bottom-32 md:w-20 md:h-20 h-10 w-10 border-8 border-blue-500 "></div>
 
         <Image
           className="z-50"
